1d4783161a4eb1de463d6064f9f7cc4514e6ae91
Nate <nate@calculated.fi>
2024-12-09 14:22:32 +1100

    testing deployment to vercel

--- test-frontend-engineer/next.config.ts
+++ test-frontend-engineer/next.config.ts
@@ -1,6 +1,4 @@
-import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const nextConfig: NextConfig = {
-};
-
-export default nextConfig;
+module.exports = nextConfig;
